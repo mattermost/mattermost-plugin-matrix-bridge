adf80aafae2e267acad7c3b1dc7b01276c3610ed
wiggin77 <wiggin77@warpmail.net>
2025-06-07 01:16:07 -0400

    better help text

--- server/command/command_test.go
+++ server/command/command_test.go
@@ -7,11 +7,21 @@ import (
 	"github.com/mattermost/mattermost/server/public/plugin/plugintest"
 	"github.com/mattermost/mattermost/server/public/pluginapi"
 	"github.com/stretchr/testify/assert"
+	"github.com/wiggin77/mattermost-plugin-matrix-bridge/server/matrix"
+	"github.com/wiggin77/mattermost-plugin-matrix-bridge/server/store/kvstore"
 )
 
 type env struct {
 	client *pluginapi.Client
 	api    *plugintest.API
+}
+
+type mockConfiguration struct {
+	serverURL string
+}
+
+func (m *mockConfiguration) GetMatrixServerURL() string {
+	return m.serverURL
 }
 
 func setupTest() *env {
@@ -29,6 +39,7 @@ func TestHelloCommand(t *testing.T) {
 	assert := assert.New(t)
 	env := setupTest()
 
+	// Expect both command registrations
 	env.api.On("RegisterCommand", &model.Command{
 		Trigger:          helloCommandTrigger,
 		AutoComplete:     true,
@@ -36,7 +47,30 @@ func TestHelloCommand(t *testing.T) {
 		AutoCompleteHint: "[@username]",
 		AutocompleteData: model.NewAutocompleteData("hello", "[@username]", "Username to say hello to"),
 	}).Return(nil)
-	cmdHandler := NewCommandHandler(env.client)
+
+	matrixData := model.NewAutocompleteData(matrixCommandTrigger, "[subcommand]", "Matrix bridge commands")
+	matrixData.AddCommand(model.NewAutocompleteData("test", "", "Test Matrix connection"))
+	matrixData.AddCommand(model.NewAutocompleteData("create", "[room_name]", "Create a new Matrix room and map to current channel"))
+	matrixData.AddCommand(model.NewAutocompleteData("map", "[room_alias|room_id]", "Map current channel to Matrix room (prefer #alias:server.com)"))
+	matrixData.AddCommand(model.NewAutocompleteData("list", "", "List all channel-to-room mappings"))
+	matrixData.AddCommand(model.NewAutocompleteData("status", "", "Show bridge status"))
+
+	env.api.On("RegisterCommand", &model.Command{
+		Trigger:          matrixCommandTrigger,
+		AutoComplete:     true,
+		AutoCompleteDesc: "Matrix bridge commands",
+		AutoCompleteHint: "[subcommand]",
+		AutocompleteData: matrixData,
+	}).Return(nil)
+
+	// Create mock dependencies
+	mockKVStore := kvstore.NewKVStore(env.client)
+	mockMatrixClient := matrix.NewClient("http://test.com", "test_token")
+	mockGetConfig := func() Configuration {
+		return &mockConfiguration{serverURL: "http://test.com"}
+	}
+	
+	cmdHandler := NewCommandHandler(env.client, mockKVStore, mockMatrixClient, mockGetConfig)
 
 	args := &model.CommandArgs{
 		Command: "/hello world",
